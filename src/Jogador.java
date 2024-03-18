e0dff2d981dd19c8239155baf663a067f6bd4c50
CherylHenkels <cherylhenkelss@gmail.com>
2024-03-18 09:55:30 -0300

    M1S05: Ex 2 - Adicione encapsulamento ao Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,9 +1,50 @@
 public class Jogador {
-    String nome;
-    int idade;
-    int pontuacao = 0;
-    int numeroTentivas = 0;
+    private String nome;
+    private int idade;
+    private int pontuacao = 0;
+    private int numeroTentativas = 0;
 
+    public Jogador(String nome,
+        int idade,
+        int pontuacao,
+        int numeroTentativas){
+        this.nome = nome;
+        this.idade = idade;
+        this.pontuacao = pontuacao;
+        this.numeroTentativas = numeroTentativas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public int getNumeroTentativas() {
+        return numeroTentativas;
+    }
+
+    public void setNumeroTentativas(int numeroTentivas) {
+        this.numeroTentativas = numeroTentivas;
+    }
 
     public void adicionaPontos(int pontosGanhos){
         this.pontuacao += pontosGanhos;
@@ -14,7 +55,7 @@ public class Jogador {
     }
 
     public void adicionaTentativa(){
-        this.numeroTentivas++;
+        this.numeroTentativas++;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Jogador jogador = new Jogador();
+        Jogador jogador = new Jogador("Bob", 24, 0,0);
 
-        // Novo jogador
-        jogador.nome = "Bob";
-        jogador.idade = 20;
+        // Acesso aos atributos utilizando os getters
+        System.out.println("Nome: " + jogador.getNome());
+        System.out.println("Idade: " + jogador.getIdade());
+        System.out.println("Pontuação: " + jogador.getPontuacao());
+        System.out.println("Tentativas: " + jogador.getNumeroTentativas());
 
         // Tentativa 1
         jogador.adicionaTentativa();
@@ -15,9 +17,23 @@ public class Main {
         jogador.adicionaTentativa();
         jogador.perdePontos(1);
 
-        // Resultado
-        System.out.println("Número de tentativas: " + jogador.numeroTentivas);
-        System.out.println("Pontuação final: " + jogador.pontuacao);
+        // Acesso aos atributos atualizados
+        System.out.println("Nova Pontuação: " + jogador.getPontuacao());
+        System.out.println("Novas Tentativas: " + jogador.getNumeroTentativas());
+
+        // Modificação dos atributos utilizando os setters
+        jogador.setPontuacao(150);
+        jogador.setNumeroTentativas(7);
+
+        // Acesso aos atributos atualizados
+        System.out.println("Nova Pontuação: " + jogador.getPontuacao());
+        System.out.println("Novas Tentativas: " + jogador.getNumeroTentativas());
+
+
+
+
+
+
 
     }
 }
