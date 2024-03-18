8a2b6665c54c640094333e5dc8b066878fda22c9
CherylHenkels <cherylhenkelss@gmail.com>
2024-03-18 15:18:23 -0300

    M1S05: Ex 6 - Crie o Jogo

---
+++ src/Jogo.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Jogo {
+
+    private Jogador melhorJogador;
+    private int numeroJogadas;
+
+    public Jogo(Jogador melhorJogador) {
+        this.melhorJogador = melhorJogador;
+        this.numeroJogadas = 0;
+    }
+
+    public void jogar() {
+        Scanner scanner = new Scanner(System.in);
+        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+
+        while (true) {
+            System.out.println("Digite ");
+            System.out.println("1 - Pedra");
+            System.out.println("2 - Papel");
+            System.out.println("3 - Tesoura");
+            System.out.println("0 - Sair");
+            int jogadaJogador = scanner.nextInt();
+            scanner.nextLine();
+
+            // Verificar se o jogador escolheu uma opção válida
+            if (jogadaJogador == 0){
+                break;
+            } else if (jogadaJogador != 1 && jogadaJogador != 2 && jogadaJogador != 3 && jogadaJogador != 0 ) {
+                System.out.println("Jogada inválida.");
+                continue;
+            }
+
+            // Número aleatório entre 0 e 1. Multiplicado pelo tamanho do array opcoes. Casting para número inteiro
+            int jogadaComputador = ((int) (Math.random() * opcoes.length)) + 1 ;
+
+            // Imprime as jogadas
+            System.out.println("Você jogou: " + " " + jogadaJogador + " " + opcoes[jogadaJogador - 1]);
+            System.out.println("O computador jogou: " + " " + jogadaComputador + " "  + opcoes[jogadaComputador - 1]);
+
+            // Verifica o resultado do jogo
+            if (jogadaJogador == jogadaComputador) {
+                System.out.println("Empate!");
+            } else if (jogadaJogador == (jogadaComputador + 1)){
+                System.out.println("Você ganhou!");
+                melhorJogador.setPontuacao(melhorJogador.getPontuacao() + 1);
+            } else if (jogadaJogador == 1 && jogadaComputador == 3){
+                System.out.println("Você ganhou!");
+                melhorJogador.setPontuacao(melhorJogador.getPontuacao() + 1);
+            } else {
+                System.out.println("Você perdeu!");
+            }
+
+            // Incrementa o número de jogadas e tentativas
+            numeroJogadas++;
+            melhorJogador.setNumeroTentativas(melhorJogador.getNumeroTentativas() + 1);
+
+            // Pergunta ao jogador se ele quer jogar novamente
+            System.out.println("Deseja jogar novamente? (s/n)");
+            String continuar = scanner.nextLine();
+            if (!continuar.equalsIgnoreCase("s")) {
+                break;
+            }
+        }
+
+        // Imprime o número total de jogadas e a pontuação do melhor jogador
+        System.out.println("Número total de jogadas: " + numeroJogadas);
+        System.out.println("Pontuação do melhor jogador: " + melhorJogador.getPontuacao());
+    }
+}
---
+++ src/MainExercicio6.java
@@ -0,0 +1,7 @@
+public class MainExercicio6 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cheryl", 26, 0,0);
+        Jogo jogo = new Jogo(jogador);
+        jogo.jogar();
+    }
+}
